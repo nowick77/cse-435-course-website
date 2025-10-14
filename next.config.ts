69d9459d0e60c2826119dc7f7bb3b2b3c5d0f94d
jackA2SC <jack@a2systemsconsulting.com>
2025-10-13 21:02:53 -0400

    vercel transition

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/cse-435-course-website',
-    assetPrefix: '/course-website/',
-    images: {
-        unoptimized: true,
-    },
 }
 
 module.exports = nextConfig
